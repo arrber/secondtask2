03b7646306edb30a4a99443e4ac6ab34e5558c81
arber <arberbreznica98@gmail.com>
2022-02-05 02:32:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [active, setActive] = useState(false);
+
+  const openAccordion = () => {
+    if(active == false){
+      setActive(true);
+    }
+    else{
+      setActive(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="accordion-holder">
+        <div onClick={openAccordion} className="accordion">Why park a domain name in Parkname?<span className="arrow">{active ? "^" : "˅"}</span>
+          {
+            active &&
+            <p className="content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Quis illum facere tempora dolore minus voluptas omnis quidem reiciendis expedita amet odio, qui, 
+              quasi at magni est in sint, voluptates consectetur.
+            </p>
+          }
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
